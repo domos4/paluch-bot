b60457a7b67335ec26e9337adf529427bcf0b882
Dominik Chmielarz <dominik.chmielarz@codility.com>
2020-11-25 23:48:58 +0100

    commit

--- get-pet-ids.zsh
+++ get-pet-ids.zsh
@@ -1,7 +1,7 @@
 DATE=$(date)
-wget -O "napaluchu1.html $DATE" https://napaluchu.waw.pl/zwierzeta/zwierzeta-do-adopcji/\?pet_breed\=0\&pet_sex\=0\&pet_weight\=2\&pet_age\=1\&pet_date_from\=\&pet_date_to\=\&pet_name\=\&submit-form\= 
-wget -O "napaluchu2.html $DATE" https://napaluchu.waw.pl/zwierzeta/zwierzeta-do-adopcji/\?pet_breed\=0\&pet_sex\=0\&pet_weight\=2\&pet_age\=2\&pet_date_from\=\&pet_date_to\=\&pet_name\=\&submit-form\=
-cat "napaluchu1.html $DATE" | grep -Eo '<a href="/pet/(\d+)/">dowiedz' | grep -Eo '\d+' | tee "napaluchu1.db $DATE"
-cat "napaluchu2.html $DATE" | grep -Eo '<a href="/pet/(\d+)/">dowiedz' | grep -Eo '\d+' | tee "napaluchu2.db $DATE"
-cat "napaluchu1.db $DATE" | xargs deno run --allow-read --allow-write update-db.ts
-cat "napaluchu2.db $DATE" | xargs deno run --allow-read --allow-write update-db.ts
+wget -O "napaluchu_html1_$DATE" https://napaluchu.waw.pl/zwierzeta/zwierzeta-do-adopcji/\?pet_breed\=0\&pet_sex\=0\&pet_weight\=2\&pet_age\=1\&pet_date_from\=\&pet_date_to\=\&pet_name\=\&submit-form\= 
+wget -O "napaluchu_html2_$DATE" https://napaluchu.waw.pl/zwierzeta/zwierzeta-do-adopcji/\?pet_breed\=0\&pet_sex\=0\&pet_weight\=2\&pet_age\=2\&pet_date_from\=\&pet_date_to\=\&pet_name\=\&submit-form\=
+cat "napaluchu_html1_$DATE" | grep -Eo '<a href="/pet/(\d+)/">dowiedz' | grep -Eo '\d+' | tee "napaluchu_db1_$DATE"
+cat "napaluchu_html2_$DATE" | grep -Eo '<a href="/pet/(\d+)/">dowiedz' | grep -Eo '\d+' | tee "napaluchu_db2_$DATE"
+cat "napaluchu_db1_$DATE" | xargs node update-db.js --date="$(date)"
+cat "napaluchu_db2_$DATE" | xargs node update-db.js --date="$(date)"
