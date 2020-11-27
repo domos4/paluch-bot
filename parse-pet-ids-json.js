f349ad8dfc3537366367e22567109d2f6dc3d42d
Dominik Chmielarz <dominik.chmielarz@codility.com>
2020-11-28 00:29:24 +0100

    commit

--- parse-pet-ids-json.js
+++ parse-pet-ids-json.js
@@ -3,7 +3,9 @@ const fs = require('fs');
 function parsePetIdsJson(jsonPath) {
   try {
     const petIdsJson = fs.readFileSync(jsonPath);
-    return JSON.parse(petIdsJson).ids || [];
+    const petIds = JSON.parse(petIdsJson).ids || [];
+    console.log(`parsed pet ids for json path: ${jsonPath}`, petIds);
+    return petIds;
   } catch (error) {
     return [];
   }
