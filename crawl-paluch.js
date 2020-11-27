d770a54e93f9f2a8a1e70de748debb19837560e8
Dominik Chmielarz <dominik.chmielarz@codility.com>
2020-11-27 21:28:20 +0100

    commit

--- crawl-paluch.js
+++ crawl-paluch.js
@@ -13,12 +13,16 @@ try {
 execSync(`mkdir ${dataPath}`);
 
 function fetchData(url, id) {
-  const numberOfPages = 1;
-  for (let pageIndex = 0; pageIndex < numberOfPages; pageIndex++) {
+  const numberOfPages = 10;
+  for (let pageIndex = 1; pageIndex <= numberOfPages; pageIndex++) {
     const htmlFileName = `${dataPath}/index_${id}_${pageIndex}.html`;
     const dbFileName = `${dataPath}/db_${id}_${pageIndex}`;
-    execSync(`wget -O "${htmlFileName}" ${url}`);
-    execSync(`cat "${htmlFileName}" | grep -Eo '<a href="/pet/(\\d+)/">dowiedz' | grep -Eo '\\d+' | tee "${dbFileName}"`);
+    const fetchHtmlCmd = `wget -O "${htmlFileName}" ${url}&pet_page=${pageIndex}`;
+    console.log(fetchHtmlCmd);
+    execSync(fetchHtmlCmd);
+    const saveIdsToDbCmd = `cat "${htmlFileName}" | grep -Eo '<a href="/pet/(\\d+)/">dowiedz' | grep -Eo '\\d+' | tee "${dbFileName}"`;
+    console.log(saveIdsToDbCmd);
+    execSync(saveIdsToDbCmd);
     const db = fs.readFileSync(dbFileName).toString().split("\n");
     updateMasterDb(db.slice(0, db.length - 1), `${dataPath}/new-pet-ids_${id}_${pageIndex}.json`);
   }
@@ -27,4 +31,4 @@ function fetchData(url, id) {
 const url = 'https://napaluchu.waw.pl/zwierzeta/zwierzeta-do-adopcji/';
 
 fetchData(url + '?pet_species=1&pet_weight=2&pet_age=1', '1');
-// fetchData(url + '?pet_species=1&pet_weight=2&pet_age=2', '2');
+fetchData(url + '?pet_species=1&pet_weight=2&pet_age=2', '2');
