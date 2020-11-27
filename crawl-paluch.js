179c7f9a9ea95968be08280fa2f6b849bdf63d09
Dominik Chmielarz <dominik.chmielarz@codility.com>
2020-11-27 21:46:50 +0100

    commit

--- crawl-paluch.js
+++ crawl-paluch.js
@@ -3,21 +3,19 @@ const { execSync } = require('child_process');
 const updateMasterDb = require('./update-db');
 
 const time = new Date().getTime();
-const dataPath = `./data/${time}`;
+const dataParentPath = `./data/${time}`;
 
-try {
-  execSync(`mkdir data`);
-} catch (error) {
-  //ignore
-}
-execSync(`mkdir ${dataPath}`);
+execSync(`mkdir -p ${dataParentPath}`);
 invoke();
 
 async function fetchData(url, id) {
   const numberOfPages = 10;
   for (let pageIndex = 1; pageIndex <= numberOfPages; pageIndex++) {
-    const htmlFileName = `${dataPath}/index_${id}_${pageIndex}.html`;
-    const dbFileName = `${dataPath}/db_${id}_${pageIndex}`;
+    const pageDirName = `page${pageIndex}`;
+    const dataPath = `${dataParentPath}/${id}/${pageDirName}`;
+    execSync(`mkdir -p ${dataPath}`);
+    const htmlFileName = `${dataPath}/index.html`;
+    const dbFileName = `${dataPath}/db`;
     const fetchHtmlCmd = `wget -O "${htmlFileName}" "${url}&pet_page=${pageIndex}"`;
     console.log(fetchHtmlCmd);
     execSync(fetchHtmlCmd);
@@ -25,12 +23,12 @@ async function fetchData(url, id) {
     console.log(saveIdsToDbCmd);
     execSync(saveIdsToDbCmd);
     const db = fs.readFileSync(dbFileName).toString().split("\n");
-    await updateMasterDb(db.slice(0, db.length - 1), `${dataPath}/new-pet-ids_${id}_${pageIndex}.json`);
+    await updateMasterDb(db.slice(0, db.length - 1), `${dataPath}/new-pet-ids.json`);
   }
 }
 
 async function invoke() {
   const url = 'https://napaluchu.waw.pl/zwierzeta/zwierzeta-do-adopcji/';
-  await fetchData(url + '?pet_species=1&pet_weight=2&pet_age=1', '1');
-  await fetchData(url + '?pet_species=1&pet_weight=2&pet_age=2', '2');
+  await fetchData(url + '?pet_species=1&pet_weight=2&pet_age=1', 'query1');
+  await fetchData(url + '?pet_species=1&pet_weight=2&pet_age=2', 'query2');
 }
