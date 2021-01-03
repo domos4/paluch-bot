55fa0312a50f65c12a78beda52498d0367a9f916
Dominik Chmielarz <dominik.chmielarz@codility.com>
2021-01-03 19:39:28 +0100

    commit

--- check-for-new-pet-ids-and-notify.sh
+++ check-for-new-pet-ids-and-notify.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-node src/crawl-paluch.js dog --preserve-data --verbose --pages-count 30
-node src/crawl-paluch.js cat --preserve-data --verbose --pages-count 30
+NODE_ENV=production node src/crawl-paluch.js dog --preserve-data --verbose --pages-count 1
+NODE_ENV=production node src/crawl-paluch.js cat --preserve-data --verbose --pages-count 1
