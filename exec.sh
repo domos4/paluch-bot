09cd0b4f846da011181e133056d131f31fcd1d3e
Dominik Chmielarz <dominik.chmielarz@codility.com>
2021-01-04 17:25:58 +0100

    make NODE_ENV optional

--- exec.sh
+++ exec.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 yarn
-NODE_ENV=production node src/crawl-paluch.js cat --pages-count 50
-NODE_ENV=production node src/crawl-paluch.js dog --pages-count 50
+$@ node src/crawl-paluch.js cat --pages-count 50
+$@ node src/crawl-paluch.js dog --pages-count 50
