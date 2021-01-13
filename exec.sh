3f3f15345bb4cdf9aca5ce83c5df05a0af25932d
Dominik Chmielarz <dominik.chmielarz@codility.com>
2021-01-14 00:17:05 +0100

    update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-yarn
+PGHOST=localhost \
+PGDATABASE=napaluchu \
+PGPORT=5432 \
+$@ \
+node src/save-new-pets-and-notify.js cat --pages-count 1 --verbose
 
 PGHOST=localhost \
 PGDATABASE=napaluchu \
 PGPORT=5432 \
 $@ \
-node src/save-new-pets-and-notify.js cat --pages-count 5 --verbose
-
-PGHOST=localhost \
-PGDATABASE=napaluchu \
-PGPORT=5432 \
-$@ \
-node src/save-new-pets-and-notify.js dog --pages-count 5 --verbose
+node src/save-new-pets-and-notify.js dog --pages-count 1 --verbose
